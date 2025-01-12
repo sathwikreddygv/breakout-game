addb19abab18d90c3ebee4853700293f6c370abc
sathwikreddygv <sathwikreddygv@gmail.com>
2025-01-12 12:40:20 +0530

    ll

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,7 +1,7 @@
 import { Boot } from './scenes/Boot';
 // import { GameOver } from './scenes/GameOver';
 // import { Game as MainGame } from './scenes/Game';
-import { AUTO, Game } from 'phaser';
+import { AUTO, Game, Scale } from 'phaser';
 // import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
@@ -20,6 +20,20 @@ const config: Phaser.Types.Core.GameConfig = {
         arcade: {
             gravity: {y : 0, x:0}
         }
+    },
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH,
+        // Optional: set minimum and maximum dimensions
+        min: {
+            width: 320,
+            height: 240
+        },
+        max: {
+            width: 1600,
+            height: 1200
+        },
+        zoom: 1
     }
 };
 
